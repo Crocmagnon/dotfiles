3de866694926fd72ae3536303b3f5d97d0c0272d
Gabriel Augendre <gabriel@augendre.info>
2022-03-21 14:06:28 +0100

    Update kdjango

--- dot_config/fish/private_functions/private_kdjango.fish
+++ dot_config/fish/private_functions/private_kdjango.fish
@@ -1,11 +1,12 @@
 # Defined interactively
 function kdjango --description 'Pop into the shell of django pod of the current namespace'
-    argparse -x "h,b,s,m" "h/help" "b/bash" "s/sh" "m/manage" -- $argv; or return
+    argparse -x "h,b,s,m" "h/help" "b/bash" "s/sh" "m/manage" "o/original-shell" -- $argv; or return
     if set -q _flag_h
         echo "Exec into the django pod of the current namespace..."
         echo -e "kdjango [-b]\t ...using bash (default if no option)"
         echo -e "kdjango -s\t ...using sh"
-        echo -e "kdjango -m\t ...using manage.py shell"
+        echo -e "kdjango -m\t ...using manage.py shell_plus"
+        echo -e "kdjango -o\t ...using manage.py shell"
         echo -e "\nRequires kubens for namespace selection"
         return
     end
@@ -15,6 +16,8 @@ function kdjango --description 'Pop into the shell of django pod of the current 
     else if set -q _flag_s
         set command /bin/sh
     else if set -q _flag_m
+        set command ./manage.py shell_plus
+    else if set -q _flag_o
         set command ./manage.py shell
     end
     set pod (kubectl get pods -o name | grep "django" | grep -Ev "celery|migrations" | head -1)
