60b2891ab127af3b056b8908298b1a842cd593bc
Gabriel Augendre <gabriel@augendre.info>
2022-03-30 23:50:15 +0200

    Update book notes path

--- dot_config/fish/private_functions/private_kobosavehl.fish
+++ dot_config/fish/private_functions/private_kobosavehl.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function kobosavehl
-cp -R /Volumes/KOBOeReader/.adds/koreader/clipboard/ "/Users/gaugendre/Nextcloud/DesktopSync/Documents/Perso/Book notes/"
+cp -R /Volumes/KOBOeReader/.adds/koreader/clipboard/ "/Users/gaugendre/Nextcloud/Perso/50-59 - Divertissements & projets/51 - Mes lectures/51.02 - Book notes/"
 end
