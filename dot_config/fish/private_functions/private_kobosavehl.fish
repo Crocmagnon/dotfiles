2e10f7e1fbb7835c911f2417e9a564b99843c34c
Gabriel Augendre <gabriel@augendre.info>
2022-03-31 00:36:53 +0200

    Pimp kobo save hl script

--- dot_config/fish/private_functions/private_kobosavehl.fish
+++ dot_config/fish/private_functions/private_kobosavehl.fish
@@ -1,4 +1,7 @@
 # Defined interactively
 function kobosavehl
-cp -R /Volumes/KOBOeReader/.adds/koreader/clipboard/ "/Users/gaugendre/Nextcloud/Perso/50-59 - Divertissements & projets/51 - Mes lectures/51.02 - Book notes/"
+    pushd "/Users/gaugendre/Nextcloud/Perso/50-59 - Divertissements & projets/51 - Mes lectures/51.02 - Book notes"
+    cp -R /Volumes/KOBOeReader/.adds/koreader/clipboard/ ./
+    python ./parse.py -f KOReaderClipping.json
+    diskutil eject KOBOeReader
 end
