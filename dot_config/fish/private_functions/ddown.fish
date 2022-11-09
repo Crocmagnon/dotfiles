3b13a0ff0e8a86045b28de25ff38dfb1dccd6f94
Gabriel Augendre <gabriel@augendre.info>
2022-11-09 15:34:37 +0100

    Fix ddown function for compose v2

--- dot_config/fish/private_functions/ddown.fish
+++ dot_config/fish/private_functions/ddown.fish
@@ -1,5 +1,5 @@
 function ddown
-set -l workdir (docker inspect -f '{{ index .Config.Labels "com.docker.compose.project.working_dir" }}' (docker ps -q | head -n1))
-set -l compose_file (docker inspect -f '{{ index .Config.Labels "com.docker.compose.project.config_files" }}' (docker ps -q | head -n1))
-docker-compose -f "$workdir/$compose_file" down
+    #set -l workdir (docker inspect -f '{{ index .Config.Labels "com.docker.compose.project.working_dir" }}' (docker ps -q | head -n1))
+    set -l compose_file (docker inspect -f '{{ index .Config.Labels "com.docker.compose.project.config_files" }}' (docker ps -q | head -n1))
+    docker compose -f "$compose_file" down
 end
