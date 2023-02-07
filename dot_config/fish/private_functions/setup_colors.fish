c25811e6143f73bad4a8dd0ca5ae464ef475523c
Gabriel Augendre <gabriel@augendre.info>
2023-02-07 10:20:38 +0100

    Add setup colors function

---
+++ dot_config/fish/private_functions/setup_colors.fish
@@ -0,0 +1,32 @@
+function setup_colors
+    set -U fish_color_autosuggestion 555 brblack
+    set -U fish_color_cancel -r
+    set -U fish_color_command --bold
+    set -U fish_color_comment red
+    set -U fish_color_cwd green
+    set -U fish_color_cwd_root red
+    set -U fish_color_end brmagenta
+    set -U fish_color_error brred
+    set -U fish_color_escape bryellow --bold
+    set -U fish_color_history_current --bold
+    set -U fish_color_host normal
+    set -U fish_color_host_remote yellow
+    set -U fish_color_match --background=brblue
+    set -U fish_color_normal normal
+    set -U fish_color_operator bryellow
+    set -U fish_color_param cyan
+    set -U fish_color_quote yellow
+    set -U fish_color_redirection brblue
+    set -U fish_color_search_match bryellow --background=brblack
+    set -U fish_color_selection white --bold --background=brblack
+    set -U fish_color_status red
+    set -U fish_color_user brgreen
+    set -U fish_color_valid_path --underline
+    set -U fish_pager_color_completion 
+    set -U fish_pager_color_description B3A06D yellow
+    set -U fish_pager_color_prefix white --bold --underline
+    set -U fish_pager_color_progress brwhite --background=cyan
+    set -U fish_pager_color_selected_background -r
+    echo "[31mF[31mi[31ms[31mh[31m [31mc[38;2;255;136;0mo[38;2;255;136;0ml[38;2;255;136;0mo[38;2;255;136;0mr[33ms[33m [33mh[33ma[32mv[32me[32m [32mb[34me[34me[34mn[34m [35ms[35me[35mt(B[m"
+end
+
