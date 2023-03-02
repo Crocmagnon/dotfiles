76ff1c5b1841969936a93dfd4118588ea61853b1
Gabriel Augendre <gabriel@augendre.info>
2023-03-02 18:29:34 +0100

    Ask for root password at the beginning of upgrade

--- dot_config/fish/private_functions/upgrade.fish
+++ dot_config/fish/private_functions/upgrade.fish
@@ -1,9 +1,10 @@
 function upgrade --wraps=topgrade
-if not bw login --check
-set -x BW_SESSION (bw login --raw)
-end
-if not bw unlock --check
-set -x BW_SESSION (bw unlock --raw)
-end
-topgrade
+    if not bw login --check
+        set -x BW_SESSION (bw login --raw)
+    end
+    if not bw unlock --check
+        set -x BW_SESSION (bw unlock --raw)
+    end
+    sudo ls >/dev/null
+    topgrade
 end
