ab9f724c28b8ef938de64ce8c7a45ab7858aecdb
Gabriel Augendre <gabriel@augendre.info>
2024-02-19 20:40:28 +0100

    neovim: update on-save triggers for go

--- dot_config/nvim/lua/custom/configs/null-ls.lua
+++ dot_config/nvim/lua/custom/configs/null-ls.lua
@@ -4,8 +4,8 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local opts = {
   sources = { -- run these on save
     null_ls.builtins.formatting.gofumpt,
-    null_ls.builtins.formatting.goimports_reviser,
-    null_ls.builtins.formatting.golines,
+    null_ls.builtins.formatting.goimports,
+    null_ls.builtins.diagnostics.golangci_lint,
   },
   on_attach = function (client, bufnr)
     if client.supports_method("textDocument/formatting") then
@@ -17,7 +17,7 @@ local opts = {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({bufnr = bufnr})
+          vim.lsp.buf.format({ async = false })
         end,
       })
     end
