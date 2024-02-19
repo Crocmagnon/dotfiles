7598762fccd545e83ee23f5245697d1145153e83
Gabriel Augendre <gabriel@augendre.info>
2024-02-20 00:56:31 +0100

    neovim: enable incremental selection

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -8,6 +8,15 @@ local plugins = {
         "python",
       },
       auto_install = true,
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
     },
   },
   {
