9af3b97d36dbee3f86a59fdc665a1ead59e293db
Gabriel Augendre <gabriel@augendre.info>
2024-11-17 11:44:09 +0100

    move to iterm script & add function to update nvim theme

--- dot_config/fish/private_functions/update_theme.fish
+++
@@ -1,7 +0,0 @@
-function update_theme --on-variable macOS_Theme
-    if [ "$macOS_Theme" = "dark" ]
-        yes | fish_config theme save "Catppuccin Mocha"
-    else if [ "$macOS_Theme" = "light" ]
-        yes | fish_config theme save "Catppuccin Latte"
-    end
-end
