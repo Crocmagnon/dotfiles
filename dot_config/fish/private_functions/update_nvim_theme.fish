fa7faa1315d10fe212e70204d3d08d97505fbd6d
Gabriel Augendre <gabriel@augendre.info>
2024-11-19 18:00:47 +0100

    move nvim tmp dir to var

--- dot_config/fish/private_functions/update_nvim_theme.fish
+++ dot_config/fish/private_functions/update_nvim_theme.fish
@@ -1,5 +1,5 @@
 function update_nvim_theme --description 'Update neovim theme with light/dark mode' --argument theme
-for addr in /var/folders/7r/l55lwv013hl_g8gc971k7kgw0000gn/T/nvim.gaugendre/**/*
+for addr in $NVIM_TMP_DIR/**/*
 nvim --server $addr --remote-send ":set background=$theme <cr>"
 end
 end
