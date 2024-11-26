00495ccacf21c4699d9e74f809d922d2f30c79e8
Gabriel Augendre <gabriel@augendre.info>
2024-11-26 15:17:16 +0100

    move bat theme to light/dark functions

--- dot_config/fish/private_functions/dark.fish
+++ dot_config/fish/private_functions/dark.fish
@@ -1,4 +1,5 @@
 function dark --description 'Set shell theme to dark mode, including neovim'
 yes | fish_config theme save "Catppuccin Mocha"
+set -Ux BAT_THEME "Catppuccin Mocha"
 update_nvim_theme dark
 end
--- dot_config/fish/private_functions/light.fish
+++ dot_config/fish/private_functions/light.fish
@@ -1,4 +1,5 @@
 function light --description 'Set shell theme to light mode, including neovim'
 yes | fish_config theme save "Catppuccin Latte"
+set -Ux BAT_THEME "Catppuccin Latte"
 update_nvim_theme light
 end
