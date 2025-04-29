a35a31ba44f26e86b0420675ab5b977219f52718
Gabriel Augendre <gabriel.augendre@ovhcloud.com>
2025-04-29 17:16:12 +0200

    fix keepass funcs for linux

--- dot_config/fish/private_functions/interne.fish
+++ dot_config/fish/private_functions/interne.fish
@@ -1,5 +1,5 @@
 function interne
-  set creds (keepassxc-cli show -a username -a password ~/Nextcloud-OVH/keepass.kdbx Interne)
+  set creds (keepassxc-cli show -a username -a password ~/Nextcloud/keepass.kdbx Interne)
   set -U INTERNE_USER $creds[1]
   set -U INTERNE_PWD $creds[2]
 end
