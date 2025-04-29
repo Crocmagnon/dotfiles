5c2492acc5d53c33fdc1a80f00baff0d2b80c6b9
Gabriel Augendre <gabriel.augendre@ovhcloud.com>
2025-04-29 17:09:41 +0200

    fix keepass funcs for mac

--- dot_config/fish/private_functions/corp.fish
+++ dot_config/fish/private_functions/corp.fish
@@ -1,5 +1,5 @@
 function corp
-  set creds (keepassxc-cli show -a username -a password ~/Nextcloud/OVH/keepass.kdbx corp)
+  set creds (keepassxc-cli show -a username -a password ~/Nextcloud-OVH/keepass.kdbx corp)
   set -g CORP_USER $creds[1]
   set -g CORP_PWD $creds[2]
 end
